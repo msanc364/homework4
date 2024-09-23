fefeb66f9d06289564dfe9b0af08a2ee24a96f05
Michael Sanchez <msanc364@stu.southtexascollege.edu>
2024-09-22 21:10:46 -0500

    Completed transaction list Component

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,101 @@
 <script setup>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import AddTransaction from './components/AddTransaction.vue';
+import TransactionList from './components/TransactionList.vue';
 
-  import Header from './components/header.vue';
-  import Balance from './components/Balance.vue';
-  import IncomeExpenses from './components/IncomeExpenses.vue';
-  import AddTransaction from './components/AddTransaction.vue';
-  import {ref, computed} from 'vue'
-  
 
-const transactions = ref([
-    {id: 1, text:'Paycheck', ammount: 700.00},
-    {id: 2, text:'Water Bill', ammount: -72.83},
-    {id: 3, text:'Electric Bill', ammount: -153.89},
-    {id: 4, text:'Returns', ammount: 20.00},
-   
-  ])
 
-  const sum = computed(()=>{
-    return transactions.value.reduce((acc, x)=>{
-      return acc+x.ammount
-    },0)
-  })
 
-  const moneyIn = computed(()=>{
-    return transactions.value
-    .filter((x)=>x.ammount>0)
-    .reduce((acc, x)=>{
-      return acc+x.ammount
-    },0)
-  })
+import {ref, computed, onMounted} from 'vue'
 
-  const moneyOut = computed(()=>{
-    return transactions.value
-    .filter((x)=>x.ammount<0)
-    .reduce((acc, x)=>{
-      return acc+x.ammount
-    },0)
-  })
+
+const transactions = ref([])
+
+
+
+const sum = computed(()=>{
+  return transactions.value.reduce((acc, x)=>{
+    return acc+x.amount
+},0)
+})
+
+
+
+
+const moneyIn = computed(()=>{ 
+  return transactions.value
+  .filter((x)=>x.amount>0) 
+  .reduce((acc, x)=>{
+    return acc+x.amount
+},0)
+
+
+})
+
+
+const moneyOut = computed(()=>{ 
+
+
+  return transactions.value
+  .filter((x)=>x.amount<0) 
+  .reduce((acc, x)=>{
+    return acc+x.amount
+
+
+
+
+},0)
+})
+
+
+const handleTransaction = (transactionData) => {
+  transactions.value.push({ 
+    text: transactionData.text, 
+    amount: transactionData.amount,   
+})
+
+
+saveToLocalStorage()
+}
+
+
+const generateID =  () =>{
+  return Math.floor(Math.random()*10000000)
+}
+
+const handleDelete = (id) =>{
+  transactions.value = transactions.value.filter((x) => x.id !== id) 
+  saveToLocalStorage()
+}
+
+
+const saveToLocalStorage = ()  => {
+localStorage.setItem('transactions', JSON.stringify(transactions.value))
+
+}
+
+
+onMounted( () => {
+  const savedTransactions = JSON.parse(localStorage.getItem('transactions'))
+
+  if(savedTransaction){}
+    transactions.value =savedTransactions
+})
+
+
 
 
 </script>
 
-
 <template>
-  <Header></Header>
-  <div class="container">
-    <Balance :total="sum"></Balance>
-    <IncomeExpenses :income="moneyIn" :expense="moneyOut"></IncomeExpenses>
-    <AddTransaction></AddTransaction>
-  </div>
+   <Header></Header>
+    <div class="container">
+        <Balance :total="sum"></Balance>
+        <IncomeExpenses :income="moneyIn" :expense="moneyOut"></IncomeExpenses>
+        <AddTransaction @transactionSubmitted="handleTransaction"></AddTransaction>
+        <TransactionList :transactions="transactions" @transactionDeleted="handleDelete"></TransactionList>
+      </div>
 
-    
 </template>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import { defineProps, defineEmits } from 'vue';
+
+    const props = defineProps({
+        transactions: {
+            type: Array,
+            Required: true,
+        }
+    })
+
+    const emit = defineEmits(['transactionDeleted'])
+
+    const deleteTransaction = (id) => {
+        emit('transactionDeleted', id)
+    }
+</script>
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li v-for="x in transactions" v-bind:key="x.id"
+        v-bind:class="x.amount < 0 ? 'minus' : 'plus'">
+        {{ x.text }} <span>${{ x.amount }}</span>
+        <button @click="deleteTransaction(x.id)" class="delete-btn">x</button>
+        </li>
+    </ul>
+</template>
