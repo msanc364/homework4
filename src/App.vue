90dfca7b072b882998decdd3229ceca121e38654
Michael Sanchez <msanc364@stu.southtexascollege.edu>
2024-09-22 19:31:09 -0500

    Finished Balance

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+  import Header from './components/header.vue';
+  import Balance from './components/Balance.vue';
+  import {ref, computed} from 'vue'
+
+const transactions = ref([
+    {id: 1, text:'Paycheck', ammount: 700.00},
+    {id: 2, text:'Water Bill', ammount: -72.83},
+    {id: 3, text:'Electric Bill', ammount: -153.89},
+    {id: 4, text:'Returns', ammount: 20.00},
+   
+  ])
+
+  const sum = computed(()=>{
+    return transactions.value.reduce((acc, x)=>{
+      return acc+x.ammount
+    },0)
+  })
+
+
+</script>
+
+
+<template>
+  <Header></Header>
+  <div class="container">
+    <Balance :total="sum"></Balance>
+  </div>
+
+    
+</template>
